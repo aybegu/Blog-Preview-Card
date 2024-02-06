ae1ede025b8e02206a488335f19e24415f981be4
Ayca Gurler <berfingurler@hotmail.com>
2024-02-06 18:19:54 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
@@ -6,15 +5,15 @@ export default function App() {
   return (
     <div className="app">
          <main className="main">
-                <img src="blog-preview-card-main/src/illustration-article.svg"/>
+                <img src= "./illustration-article.svg" alt="illustration"/>
                 <div className="main--block">
                     <h4>Learning</h4>
                 </div>
-                <p className="main--date">Published 21 Dec 2023</p>
+                <p className="main--date">Published 06 Feb 2024</p>
                 <h1 className="main--title">HTML & CSS foundations</h1>
                 <p className="main--text">These languages are the backbone of every website, defining structure, content, and presentation.</p>
                 <div className="main--profile">
-                    <img src="../blog-preview-card-main/src/image-avatar.webp" />
+                    <img src="./image-avatar.webp" alt='avatar'/>
                     <h4>Greg Hooper</h4>
                 </div>
             </main>
